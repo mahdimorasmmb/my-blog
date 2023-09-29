7ac787e338547af2ab36b9c39a05330c2924cc8c
m.morasaeiyan <m.morasaeiyan@irantalent.com>
2023-09-29 22:29:37 +0330

    fixing type

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -7,6 +7,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
+
 const Sheet = SheetPrimitive.Root
 
 const SheetTrigger = SheetPrimitive.Trigger
@@ -14,10 +15,9 @@ const SheetTrigger = SheetPrimitive.Trigger
 const SheetClose = SheetPrimitive.Close
 
 const SheetPortal = ({
-  className,
   ...props
 }: SheetPrimitive.DialogPortalProps) => (
-  <SheetPrimitive.Portal className={cn(className)} {...props} />
+  <SheetPrimitive.Portal  {...props} />
 )
 SheetPortal.displayName = SheetPrimitive.Portal.displayName
 
